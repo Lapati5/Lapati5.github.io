02c89c5a903836fb22528ee169aad1da5e4d50b7
lapati5 <1352059320@qq.com>
2020-08-15 19:30:21 +0800

    Site updated: 2020-08-15 19:30:19

---
+++ dist/music.js
@@ -0,0 +1,108 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+    
+      {
+        name: 'Canon in D',
+        artist: 'Brian Crain',
+        url: 'https://music.163.com/song/media/outer/url?id=1091088.mp3',
+        cover: 'http://p2.music.126.net/QGb9Vtyw7qHS00uEvPfM6g==/843325418547559.jpg?param=130y130',
+      },
+
+      {
+        name: '長安十二時辰',
+        artist: '村松崇継',
+        url: 'https://music.163.com/song/media/outer/url?id=1403633007.mp3',
+        cover: 'http://p2.music.126.net/YxO_3suXleqvnaKz7B-Ngg==/109951164488587913.jpg?param=130y130',
+      },
+
+      {
+        name: 'One Summer\'s Day',
+        artist: '久石譲',
+        url: 'https://music.163.com/song/media/outer/url?id=1403706788.mp3',
+        cover: 'https://p2.music.126.net/fbCNosSD3--ikl4eF9xuOQ==/109951164706053448.jpg?param=130y130',
+      },
+
+      {
+        name: 'アシタカせっ記',
+        artist: '久石譲',
+        url: 'https://music.163.com/song/media/outer/url?id=445140.mp3',
+        cover: 'https://p2.music.126.net/fbCNosSD3--ikl4eF9xuOQ==/109951164706053448.jpg?param=130y130',
+      },
+
+      {
+        name: 'Merry-Go-Round',
+        artist: '久石譲',
+        url: 'https://music.163.com/song/media/outer/url?id=1423158948.mp3',
+        cover: 'https://p2.music.126.net/fbCNosSD3--ikl4eF9xuOQ==/109951164706053448.jpg?param=130y130',
+      },
+
+      {
+        name: "海の見える街",
+        artist: '久石譲',
+        url: 'https://music.163.com/song/media/outer/url?id=33598146.mp3',
+        cover: 'https://p2.music.126.net/fbCNosSD3--ikl4eF9xuOQ==/109951164706053448.jpg?param=130y130',
+      },
+
+      {
+        name: 'Innocent',
+        artist: '久石譲',
+        url: 'https://music.163.com/song/media/outer/url?id=1409275101.mp3',
+        cover: 'https://p2.music.126.net/fbCNosSD3--ikl4eF9xuOQ==/109951164706053448.jpg?param=130y130',
+      },
+
+      {
+        name: '千尋のワルツ',
+        artist: '久石譲',
+        url: 'https://music.163.com/song/media/outer/url?id=443899.mp3',
+        cover: 'https://p2.music.126.net/fbCNosSD3--ikl4eF9xuOQ==/109951164706053448.jpg?param=130y130',
+      },
+
+      {
+        name: 'Merry Christmas Mr. Lawrence',
+        artist: '坂本龍一',
+        url: 'https://music.163.com/song/media/outer/url?id=4899152.mp3',
+        cover: 'https://p1.music.126.net/bH4YqNyJ5Yzy3mPmYLIO9w==/109951164923076509.jpg?param=130y130',
+      },
+      {
+        name: 'Where is Armo?',
+        artist: '坂本龍一',
+        url: 'https://music.163.com/song/media/outer/url?id=5059144.mp3',
+        cover: 'https://p1.music.126.net/bH4YqNyJ5Yzy3mPmYLIO9w==/109951164923076509.jpg?param=130y130',
+      },
+      {
+        name: 'Rain',
+        artist: '坂本龍一',
+        url: 'https://music.163.com/song/media/outer/url?id=22650546.mp3',
+        cover: 'https://p1.music.126.net/bH4YqNyJ5Yzy3mPmYLIO9w==/109951164923076509.jpg?param=130y130',
+      },
+      {
+        name: 'Koko',
+        artist: '坂本龍一',
+        url: 'https://music.163.com/song/media/outer/url?id=27583401.mp3',
+        cover: 'https://p1.music.126.net/bH4YqNyJ5Yzy3mPmYLIO9w==/109951164923076509.jpg?param=130y130',
+      },
+      {
+        name: 'Solitude',
+        artist: '坂本龍一',
+        url: 'https://music.163.com/song/media/outer/url?id=498210.mp3',
+        cover: 'https://p1.music.126.net/bH4YqNyJ5Yzy3mPmYLIO9w==/109951164923076509.jpg?param=130y130',
+      },
+
+      {
+        name: 'Horner: For The Love Of A Princess',
+        artist: 'James Horner',
+        url: 'https://music.163.com/song/media/outer/url?id=26828878.mp3',
+        cover: 'http://p2.music.126.net/rlzkomHJqfBIQC-eAvbQlA==/697090372014144.jpg?param=640y300',
+      },
+      {
+        name: 'The Ludlows',
+        artist: 'James Horner',
+        url: 'https://music.163.com/song/media/outer/url?id=18618065.mp3',
+        cover: 'http://p2.music.126.net/rlzkomHJqfBIQC-eAvbQlA==/697090372014144.jpg?param=640y300',
+      },
+      
+    ]
+});
